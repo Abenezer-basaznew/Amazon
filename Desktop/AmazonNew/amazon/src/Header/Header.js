e098273543a257680a63e58975820382682a1824
Abenezer basaznew <abenezerbasazne.gmail.com>
2025-03-03 13:34:33 -0800

    this is the ease,good code for the firste one

--- Desktop/AmazonNew/amazon/src/Header/Header.js
+++ Desktop/AmazonNew/amazon/src/Header/Header.js
@@ -1,61 +1,52 @@
 import React from "react";
-import { SlArrowDown } from "react-icons/sl";
+import "./Header.css";
 import { FaSearch } from "react-icons/fa";
-import { GiUsaFlag } from "react-icons/gi";
-import { FiShoppingCart } from "react-icons/fi";
+import { LuShoppingCart } from "react-icons/lu";
+
 
 const Header = () => {
-  return <div>
-      <header className="header__container">
-        <div className="header__logo">
-          <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" />
-        </div>
-        <div className="header_location">
-          <span />
-          <span>Ethiopia</span>
-        </div>
-        <div className="header__search">
-          <button>
-            all <i className="down_arrow">
-              <SlArrowDown />
-            </i>
-          </button>
-        </div>
-        <div className="header_products">
-          <div className="search_input">
-            <input type="text" name="search_product" value="search_product" />
-          </div>
-          <div className="search_container_icon">
-            <i className="search_icon">
-              <FaSearch />
-            </i>
-          </div>
-        </div>
-        <div className="header_counter">
-          <i className="america">
-            <GiUsaFlag />
-          </i>
-          <span>EN</span>
-          <i className="down_arrow2">
-            <SlArrowDown />
-          </i>
-        </div>
-        <div className="header__option">
-          <span>sign in</span>
-          <span>account & list</span>
-        </div>
-        <div className="header__option2">
-          <span>returns</span>
-          <span>& orders</span>
-        </div>
+  return (
+    <div className="header">
+    <div className="header__logo">
+      <a href="/">
+        <img
+          className="header__logo"
+          src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+          alt="amazon logo"
+        />
+      </a>
+    </div>
+    <div className="header__search">
+      <input className="header__searchInput" type="text" />
+      <i className="header__searchIcon">
+        <FaSearch />
+      </i>
+    </div>
+    <div className="header__nav">
+      <div className="header__option">
+        <span className="header__optionLineOne">Hello</span>
+        <span className="header__optionLineTwo" />
+      </div>
 
-        <div className="header_cart_icon">
-          <i className="cart">
-            <FiShoppingCart />
-          </i>
-        </div>
-      </header>
-    </div>;
-};
+      <div className="header__option">
+        <span className="header__optionLineOne">Returns</span>
+        <span className="header__optionLineTwo">& Orders</span>
+      </div>
 
-export default Header;
+      <div className="header__option">
+        <span className="header__optionLineOne">Your</span>
+        <span className="header__optionLineTwo">Prime</span>
+      </div>
+
+      <div className="header__optionBasket">
+        <i className="header__optionBasketIcon">
+          <LuShoppingCart />
+        </i>
+        
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Header
