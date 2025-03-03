cb6da5cc2d7fde2458956383e47a127f69ca2bf3
Abenezer basaznew <abenezerbasazne.gmail.com>
2025-03-03 11:36:06 -0800

    the header section are add to icon in many way and by one by one

--- Desktop/AmazonNew/amazon/src/Header/Header.js
+++ Desktop/AmazonNew/amazon/src/Header/Header.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
+import { SlArrowDown } from "react-icons/sl";
+import { FaSearch } from "react-icons/fa";
+import { GiUsaFlag } from "react-icons/gi";
+import { FiShoppingCart } from "react-icons/fi";
 
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div>
+      <header className="header__container">
+        <div className="header__logo">
+          <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" />
+        </div>
+        <div className="header_location">
+          <span />
+          <span>Ethiopia</span>
+        </div>
+        <div className="header__search">
+          <button>
+            all <i className="down_arrow">
+              <SlArrowDown />
+            </i>
+          </button>
+        </div>
+        <div className="header_products">
+          <div className="search_input">
+            <input type="text" name="search_product" value="search_product" />
+          </div>
+          <div className="search_container_icon">
+            <i className="search_icon">
+              <FaSearch />
+            </i>
+          </div>
+        </div>
+        <div className="header_counter">
+          <i className="america">
+            <GiUsaFlag />
+          </i>
+          <span>EN</span>
+          <i className="down_arrow2">
+            <SlArrowDown />
+          </i>
+        </div>
+        <div className="header__option">
+          <span>sign in</span>
+          <span>account & list</span>
+        </div>
+        <div className="header__option2">
+          <span>returns</span>
+          <span>& orders</span>
+        </div>
 
-export default Header
+        <div className="header_cart_icon">
+          <i className="cart">
+            <FiShoppingCart />
+          </i>
+        </div>
+      </header>
+    </div>;
+};
+
+export default Header;
