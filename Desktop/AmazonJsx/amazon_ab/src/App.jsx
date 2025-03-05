38e6c8954783c65700015ab5d568666eec881d26
Abenezer basaznew <abenezerbasazne.gmail.com>
2025-03-05 13:10:10 -0800

    this is the first commit of updateted file

---
+++ Desktop/AmazonJsx/amazon_ab/src/App.jsx
@@ -0,0 +1,17 @@
+import CarouselEffect from "./components/CarouselEffect/CarouselEffect"
+import Header from "./components/Header/Header"
+import LowerHeader from "./components/LowerHeader/LowerHeader"
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header/>
+      <LowerHeader />
+      <CarouselEffect />
+    </>
+  )
+}
+
+export default App
